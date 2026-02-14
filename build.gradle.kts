3a35616456df58539616a4cbfeca216dc6bff70f
Michał Kowol <michal.kowol@gmail.com>
2026-02-14 11:36:09 +0100

    Update Kotlin and Spring Boot versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 	kotlin("plugin.spring") version "2.3.10"
 	id("org.springframework.boot") version "3.5.8"
 	id("io.spring.dependency-management") version "1.1.7"
+
 }
 
 group = "com.yavaconf"
